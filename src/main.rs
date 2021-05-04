d375cb753561deb05370342bcb0d7c709e94e12f
m.zhuravlev <maks723@yandex.ru>
2021-05-04 21:58:49 +0300

    remove wasm, symphonia no ffmpeg ffi convert using local ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 use youtube_url_unwrap::Extractor;
-use std::process::exit;
-use std::fs::File;
+use std::process::{exit, Command};
+use std::fs::{File, rename};
+use tempfile::tempdir;
 use std::io::{Write, BufWriter};
+use std::ffi::OsStr;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,19 +22,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stream = Extractor::new().get_opus_stream(uid).await?;
 
     println!("{:#?}", stream);
+    println!("downloading...");
 
     let client = reqwest::Client::new();
     let res = client.get(stream.url.unwrap())
         .send().await?
         .bytes().await?;
 
-    {
-        let file = File::create("./".to_owned() + &stream.title + ".opus")
-            .expect("Unable to create file");
+    println!("prepare file");
 
-        BufWriter::new(file).write_all(&res)
-            .expect("Unable to write data");
-    }
+    let dir = tempdir()?;
+    let temp_opus = dir.path().join("temp.opus");
+    let file = File::create(&temp_opus)?;
+    BufWriter::new(&file).write_all(&res)
+        .expect("Unable to write data");
+
+    println!("{:?}", OsStr::new(&temp_opus));
+
+    let tmp_name = "tmp.mp3"; // workaround for cmd /c when need quotes
+    let command = format!("ffmpeg -i {} -vn -ar 44100 -ac 2 -b:a 192k {}", OsStr::new(&temp_opus).to_str().unwrap(), tmp_name);
+
+    println!("{:?}", &command);
+
+    Command::new("cmd")
+        .args(&["/C", &command])
+        .output()
+        .expect("failed to execute process");
+
+    rename(tmp_name,  format!("{}.mp3", stream.title));
+
+    drop(file);
+    dir.close()?;
+
+    println!("done");
 
     Ok(())
 }
