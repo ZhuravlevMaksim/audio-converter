cba888f45154244a231963e80ad4dbd223e4f983
m.zhuravlev <maks723@yandex.ru>
2021-05-04 13:29:15 +0300

    add download from link

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 use std::env;
+use youtube_url_unwrap::Extractor;
+use std::process::exit;
+use std::fs::File;
+use std::io::{Write, BufWriter};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let stream = youtube_url_unwrap::platform::get_opus_stream(args.get(1).unwrap());
+    let uid = if let Some(uid) = args.get(1) {
+        println!("Working on: {:?}", uid);
+        uid
+    } else {
+        println!("No uid arg, exit.");
+        exit(0)
+    };
 
-    println!("{:#?}", stream.await?);
+    let stream = Extractor::new().get_opus_stream(uid).await?;
+
+    println!("{:#?}", stream);
+
+    let client = reqwest::Client::new();
+    let res = client.get(stream.url.unwrap())
+        .send().await?
+        .bytes().await?;
+
+    {
+        let file = File::create("./".to_owned() + &stream.title + ".opus")
+            .expect("Unable to create file");
+
+        BufWriter::new(file).write_all(&res)
+            .expect("Unable to write data");
+    }
 
     Ok(())
-
 }
